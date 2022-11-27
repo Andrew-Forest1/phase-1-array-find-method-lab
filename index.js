92cad2fac930d406369e056f908203d0a14f4f1d
Andrew <aforest41@yahoo.com>
2022-11-27 15:11:49 -0800

    commit tree be96e0e43cfb6767ae209db36c6a08c0e7380566

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function superbowlWin(array){
+   let season = array.find(findWin);
+   if(season === undefined) {
+    return undefined;
+   }
+   else{
+    return season.year;
+   }
+}
+
+function findWin(season){
+    return season.result === "W";
+}
+
+// const record = [
+//     { year: "2015", result: "W"},
+//     { year: "2014", result: "N/A"},
+//     { year: "2013", result: "L"},
+//     //...
+//   ];
+
+//  console.log(superbowlWin(record));
